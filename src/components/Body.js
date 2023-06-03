e8af3c3e0184c843f8826a1fe7a7ddc715c1dc08
kumar-aditya <kr.aditya.msr@gmail.com>
2023-06-03 09:53:18 +0530

    setup youtube project

---
+++ src/components/Body.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import Maincontainer from './Maincontainer'
+import Sidebar from './Sidebar'
+
+const Body = () => {
+  return (
+   <div>
+    <Sidebar></Sidebar>
+    <Maincontainer></Maincontainer>
+    </div>
+  )
+}
+
+export default Body
